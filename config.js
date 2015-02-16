308b5ec3399cad355a4b0a509952c28c356ed3f5
Florent Chiron <florentchiron@gmail.com>
2015-02-16 17:08:47 +0100

    fix(config): removed trailing slash from alerting api endpoint, to avoid multiple slashes in URL path

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ module.exports = function (logger) {
 
       alerting: {
         api: {
-          endpoint: 'http://127.0.0.1:9000/'
+          endpoint: 'http://127.0.0.1:9000'
         },
 
         trigger: {},
