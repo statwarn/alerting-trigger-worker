ca99b41a2e75e4dd61c7eba02f7bd7b2702d61ed
Florent Chiron <florentchiron@gmail.com>
2015-02-16 17:01:02 +0100

    feat(AlertEntity): added basic fromJSON method

--- src/domain/AlertEntity.js
+++ src/domain/AlertEntity.js
@@ -1,8 +1,17 @@
 'use strict';
 
-
-module.exports = function () {
+module.exports = function (logger, operators, ActionEntity, TriggerEntity) {
   function AlertEntity() {}
+
+  AlertEntity.fromJSON = function (json) {
+    var alertEntity = new AlertEntity();
+    var jsonFields = ["alert_id", "name", "activated", "measurement_id"];
+
+    _.extend(alertEntity, _.pick(json, jsonFields));
+    alertEntity.triggers = _.map(json.triggers, TriggerEntity.fromJSON);
+    alertEntity.actions = _.map(json.actions, ActionEntity.fromJSON);
+    return alertEntity;
+  };
 
   return AlertEntity;
 };
