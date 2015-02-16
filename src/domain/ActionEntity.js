8404c6e415bb646461752011d247e1da06431cf9
Florent Chiron <florentchiron@gmail.com>
2015-02-16 16:50:10 +0100

    feat(ActionEntity): added basic fromJSON method

--- src/domain/ActionEntity.js
+++ src/domain/ActionEntity.js
@@ -1,7 +1,14 @@
 'use strict';
 
-
 module.exports = function () {
   function ActionEntity() {}
+
+  ActionEntity.fromJSON = function (json) {
+    var actionEntity = new ActionEntity();
+
+    _.extend(actionEntity, json);
+    return actionEntity;
+  };
+
   return ActionEntity;
 };
