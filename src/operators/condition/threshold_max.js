88c50e5104ce7cfcc9847792edc4231a32b9b77d
Florent Chiron <florentchiron@gmail.com>
2015-02-16 16:56:02 +0100

    feat(operators): added basic implementation for operators equal/threshold_max/threshold_min (not DRY yet)

---
+++ src/operators/condition/threshold_max.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = function () {
+  function OperatorThresholdMax() {}
+
+  /**
+   * Apply the operator 'threshold_max' to the given measurement data, using a target value and operator configuration
+   * @param {Object}   data          Measurement data
+   * @param {String}   targetValue   Target value, e.g. "data.foo"
+   * @param {Object}   configuration Operator configuration
+   * @param {Function} f             Callback, called with (err, match), match being true or false depending if the condition matched
+   */
+  OperatorThresholdMax.applyToData = function (data, targetValue, configuration, f) {
+    // Check if the target is valid
+    if (targetValue === "data" || !_.startsWith(targetValue, "data.")) {
+      return f(new PrettyError(500, "Invalid target '" + targetValue + "' for operator 'threshold_max'"));
+    }
+    // Check if we have the 'value' attribute in the operator configuration
+    if (!configuration || !configuration.value) {
+      return f(new PrettyError(500, "Missing configuration attribute 'value' for operator 'threshold_max'"));
+    }
+
+    var targetAttributeName = targetValue.substr("data.".length);
+    var dataFieldValue = data[targetAttributeName];
+    var expectedValue = parseInt(configuration.value, 10);
+
+    // Silently ignore the check if the data attribute we're looking for does not exist
+    if (_.isNull(dataFieldValue) || _.isUndefined(dataFieldValue)) {
+      return f(null, true);
+    }
+
+    return f(null, dataFieldValue < expectedValue);
+  };
+
+  return OperatorThresholdMax;
+};
--- src/operators/operators.js
+++ src/operators/operators.js
@@ -1,1 +1,9 @@
 'use strict';
+
+module.exports = function () {
+  return {
+    equal: require('./condition/equal')(),
+    threshold_min: require('./condition/threshold_min')(),
+    threshold_max: require('./condition/threshold_max')()
+  };
+};
